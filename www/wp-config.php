40b11f1c9f424854fd5cf96574cc5e75b0fdf5b7
Haneef Puttur <cdsputtur@gmail.com>
2021-04-07 16:15:48 +0400

    New Wordpress Version
    Updated wordpress Version, Fixed MYSQL Stop Issue

--- www/wp-config.php
+++ www/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:9322');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
